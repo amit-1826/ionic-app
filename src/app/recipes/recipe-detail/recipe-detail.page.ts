7cdcda5d0b558a059ea53fc1355df99e80c47c7d
amit-1826 <amitdubey1826@gmail.com>
2021-10-16 18:10:07 +0530

    Added recipes detail

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {RecipesService} from "../recipes.service";
-import {RecipesModel} from "../recipes.model";
-import {ActivatedRoute} from "@angular/router";
+import {RecipesService} from '../recipes.service';
+import {RecipesModel} from '../recipes.model';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,16 +12,48 @@ import {ActivatedRoute} from "@angular/router";
 export class RecipeDetailPage implements OnInit {
 
   selectedRecipe: RecipesModel;
-  constructor(private recipesService: RecipesService, private activatedRoute: ActivatedRoute) { }
+  constructor(private recipesService: RecipesService,
+              private alertCtrl: AlertController,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe((paramMap) => {
       if (!paramMap.has('recipeId')) {
+        this.router.navigate(['/recipes']);
         return;
       }
       const recipeId = paramMap.get('recipeId');
       this.selectedRecipe = this.recipesService.getRecipe(recipeId);
-    })
+    });
+  }
+
+  onDeleteRecipe() {
+    this.alertCtrl.create({
+      buttons: [
+        {
+          text: 'Cancel',
+          role:'cancel'
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.delete();
+          }
+        }
+      ],
+      header: 'Delete.?',
+      message: 'Are you sure you want to delete.?'
+    }).then((data) => {
+      if (data) {
+        data.present();
+      }
+    });
+  }
+
+  delete() {
+    this.recipesService.deleteRecipe(this.selectedRecipe.id);
+    this.router.navigate(['/recipes']);
   }
 
 }
